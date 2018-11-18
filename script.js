69e0ba1b789ad953335cbe6bff8a81bdcf2823c0
Huy Tran <khanhhuy288@gmail.com>
2018-11-18 13:11:13 +0100

    Fix minimax.

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ function emptySquares() {
 }
 
 function bestSpot() {
-    return minimax(origBoard, aiPlayer).index;
+    return findBestMove(origBoard, aiPlayer);
 }
 
 function checkTie() {
@@ -118,150 +118,198 @@ function checkTie() {
     return false;
 }
 
-// get the best move in the current board
-function minimax(newBoard, player) {
-	treeLength++;
-	// get all available spots
-	let availSpots = emptySquares(newBoard);
-
-	// return a value (-10, 0, 10) if a terminal state is found
-	// 3 terminal states:
-	// - human player wins
-	// - ai player wins
-	// - 2 players tie
-	if (checkWin(newBoard, huPlayer)) {
-		return {score: -10};
-	} else if (checkWin(newBoard, aiPlayer)) {
-		return {score: 10};
-	} else if (availSpots.length === 0) {
-		return {score: 0};
-	}
-
-	// collect move objects in array moves
-	let moves = [];
-
-	// loop through all available spots
-	for (let i = 0; i < availSpots.length; i++) {
-
-		// create an object for a spot
-		let move = {};
-		// save its index
-		move.index = newBoard[availSpots[i]];
-		// place current player in that spot
-		newBoard[availSpots[i]] = player;
-
-		// collect the score of the opponent with minimax
-		let result;
-		if (player === aiPlayer) {
-			result = minimax(newBoard, huPlayer);
-			move.score = result.score;
-		} else {
-			result = minimax(newBoard, aiPlayer);
-			move.score = result.score;
-		}
-
-		// reset the spot to empty
-		newBoard[availSpots[i]] = move.index;
-
-		// prune here
-        if ((player === aiPlayer && move.score === 10) || (player === huPlayer && move.score === -10))
-            return move;
-        else
-            moves.push(move);
-	}
-
-	// find the best move (with best score) from all the moves
-	let bestMove;
-	let bestScore;
-
-	// look for the highest score if player is ai
-	if (player === aiPlayer) {
-		bestScore = -Infinity;
-
-		for (let i = 0; i < moves.length; i++) {
-			if (moves[i].score > bestScore) {
-				bestScore = moves[i].score;
-				bestMove = i;
-			}
-		}
-	// look for the lowest score if player is human
-	} else {
-		bestScore = Infinity;
-
-		for (let i = 0; i < moves.length; i++) {
-			if (moves[i].score < bestScore) {
-				bestScore = moves[i].score;
-				bestMove = i;
-			}
-		}
-	}
-
-	// return the best move
-	return moves[bestMove];
+// check if there is any move left
+function isMoveLeft(board) {
+    for (let elem of board) {
+        if (typeof elem === 'number') {
+            return true;
+        }
+    }
+    return false;
 }
 
-// get the best move in the current board
+// evaluate the board
+// return -10 if human player wins
+// return 10 if ai player wins
+// return 0 if
+function evaluate(board) {
+    if (checkWin(board, huPlayer)) {
+        return -10;
+    } else if (checkWin(board, aiPlayer)) {
+        return 10;
+    } else if (!isMoveLeft(board)) {
+        return 0;
+    }
+}
+
+// consider all possible ways the game can go and return
+// the value of the board
+function minimax_0(board, depth, player) {
+    treeLength++;
+    let score = evaluate(board);
+
+    if (score === 10 ) {
+        return score;
+    }
+
+    if (score === -10) {
+        return score;
+    }
+
+    if (!isMoveLeft(board)) {
+        return 0;
+    }
+
+    // ai player is the maximizer
+    if (player === aiPlayer) {
+        let best = -Infinity;
+
+        for (let [i, elem] of board.entries()) {
+            if (typeof elem === 'number') {
+                // make the move
+                board[i] = player;
+
+                // call minimax recursively and choose
+                // the maximum value
+                best = Math.max(best, minimax_0(board, depth + 1, huPlayer));
+
+                // undo the move
+                board[i] = i;
+            }
+        }
+
+        return best;
+    }
+
+    // hu player is the minimizer
+    if (player === huPlayer) {
+        let best = +Infinity;
+
+        for (let [i, elem] of board.entries()) {
+            if (typeof elem === 'number') {
+                // make the move
+                board[i] = player;
+
+                // call minimax recursively and choose
+                // the minimum value
+                best = Math.min(best, minimax_0(board, depth + 1, aiPlayer));
+
+                // undo the move
+                board[i] = i;
+            }
+        }
+
+        return best;
+    }
+}
+
+// return index of the best move
+function findBestMove(board, player) {
+    let bestVal = -Infinity;
+    let index = -1;
+
+    for (let [i, elem] of board.entries()) {
+        if (typeof elem === 'number') {
+            // make the move
+            board[i] = player;
+
+            // call minimax recursively and choose
+            // the minimum value
+            let moveVal = minimax_0(board, 0, huPlayer);
+
+            // undo the move
+            board[i] = i;
+
+            // update best if current move is greater than best
+            if (moveVal > bestVal) {
+                index = i;
+                bestVal = moveVal;
+            }
+        }
+    }
+    return index;
+}
+
+// // get the best move in the current board
+// // ai is the maximizer
+// // human is the minimizer
 // function minimax(newBoard, player) {
-//     treeLength++;
-//     // get all available spots
-//     let availSpots = emptySquares(newBoard);
+// 	treeLength++;
+// 	// get all available spots
+// 	let availSpots = emptySquares(newBoard);
 //
-//     // return a value (-10, 0, 10) if a terminal state is found
-//     // 3 terminal states:
-//     // - human player wins
-//     // - ai player wins
-//     // - 2 players tie
-//     if (checkWin(newBoard, huPlayer)) {
-//         return {score: -10};
-//     } else if (checkWin(newBoard, aiPlayer)) {
-//         return {score: 10};
-//     } else if (availSpots.length === 0) {
-//         return {score: 0};
-//     }
+// 	// return a value (-10, 0, 10) if a terminal state is found
+// 	// 3 terminal states:
+// 	// - human player wins
+// 	// - ai player wins
+// 	// - 2 players tie
+// 	if (checkWin(newBoard, huPlayer)) {
+// 		return {score: -10};
+// 	} else if (checkWin(newBoard, aiPlayer)) {
+// 		return {score: 10};
+// 	} else if (availSpots.length === 0) {
+// 		return {score: 0};
+// 	}
 //
-//     // look for the highest score if player is ai
-//     if (player === aiPlayer) {
-//         let maxEval = -Infinity;
-//         let move;
-//         let theEval;
-//         for (let i = 0; i < availSpots.length; i++) {
-//             move = {};
-//             // save its index
-//             move.index = newBoard[availSpots[i]];
-//             // place current player in that spot
-//             newBoard[availSpots[i]] = player;
+// 	// collect move objects in array moves
+// 	let moves = [];
 //
-//             // collect the score of the opponent with minimax
-//             theEval = minimax(newBoard, huPlayer);
-//             maxEval = Math.max(maxEval, theEval.score);
-//             move.score = maxEval;
+// 	// loop through all available spots
+// 	for (let i = 0; i < availSpots.length; i++) {
 //
-//             // reset the spot to empty
-//             newBoard[availSpots[i]] = move.index;
-//         }
+// 		// create an object for a spot
+// 		let move = {};
+// 		// save its index
+// 		move.index = newBoard[availSpots[i]];
+// 		// place current player in that spot
+// 		newBoard[availSpots[i]] = player;
 //
-//         return move;
-//     }
-//     // look for the lowest score if player is human
-//     else {
-//         let minEval = +Infinity;
-//         let move;
-//         let theEval;
-//         for (let i = 0; i < availSpots.length; i++) {
-//             move = {};
-//             // save its index
-//             move.index = newBoard[availSpots[i]];
-//             // place current player in that spot
-//             newBoard[availSpots[i]] = player;
+// 		// collect the score of the opponent with minimax
+// 		let result;
+// 		if (player === aiPlayer) {
+// 			result = minimax(newBoard, huPlayer);
+// 			move.score = result.score;
+// 		} else {
+// 			result = minimax(newBoard, aiPlayer);
+// 			move.score = result.score;
+// 		}
 //
-//             // collect the score of the opponent with minimax
-//             theEval = minimax(newBoard, aiPlayer);
-//             minEval = Math.min(minEval, theEval.score);
-//             move.score = minEval;
+// 		// reset the spot to empty
+// 		newBoard[availSpots[i]] = move.index;
 //
-//             // reset the spot to empty
-//             newBoard[availSpots[i]] = move.index;
-//         }
-//         return move;
-//     }
+// 		// prune here
+//         if ((player === aiPlayer && move.score === 10) || (player === huPlayer && move.score === -10))
+//             return move;
+//         else
+//             moves.push(move);
+// 	}
+//
+// 	// find the best move (with best score) from all the moves
+// 	let bestMove;
+// 	let bestScore;
+//
+// 	// look for the highest score if player is ai
+// 	if (player === aiPlayer) {
+// 		bestScore = -Infinity;
+//
+// 		for (let i = 0; i < moves.length; i++) {
+// 			if (moves[i].score > bestScore) {
+// 				bestScore = moves[i].score;
+// 				bestMove = i;
+// 			}
+// 		}
+// 	// look for the lowest score if player is human
+// 	} else {
+// 		bestScore = Infinity;
+//
+// 		for (let i = 0; i < moves.length; i++) {
+// 			if (moves[i].score < bestScore) {
+// 				bestScore = moves[i].score;
+// 				bestMove = i;
+// 			}
+// 		}
+// 	}
+//
+// 	// return the best move
+// 	return moves[bestMove];
 // }
