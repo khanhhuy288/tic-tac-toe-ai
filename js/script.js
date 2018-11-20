6666d37d1a820071cc1e3ba849ab75e3ea029847
Huy Tran <khanhhuy288@gmail.com>
2018-11-21 00:09:01 +0100

    Refactor and document.

--- js/script.js
+++ js/script.js
@@ -3,108 +3,220 @@ let huPlayer;
 let aiPlayer;
 let numNodes = 0;
 let gridSize = 3;
-let winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-];
+let winCombos = getWinCombos(3);
 
 let cells = $('.cell');
 startGame();
 
+/**
+ * Generate all the win combos for a board.
+ * @param n Length of the board's edge.
+ * @returns {Array} All the win combos.
+ */
+function getWinCombos(n) {
+    let result = [];
+    let row = [];
+    let col = [];
+    let diagonalLeft = [];
+    let diagonalRight = [];
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            row.push(n*i + j);
+            col.push(i + n*j);
+        }
+        result.push(row, col);
+        row = [];
+        col = [];
+
+        diagonalLeft.push((n+1) * i);
+        diagonalRight.push((n-1) * (i+1));
+    }
+
+    result.push(diagonalLeft, diagonalRight);
+
+    return result;
+}
+
+/**
+ * Bring the board to start state.
+ */
 function startGame() {
+    // create board
     origBoard = Array.from(Array(gridSize * gridSize).keys());
-    $('.endgame').css({'display' : 'none'});
+
+    // remove highlights from previous game
     cells.text('').css({'background-color' : ''});
+
+    // make cursor over cells normal
+    cells.css({'cursor' : 'default'});
+
+    // close end game box
+    $('.endgame').css({'display' : 'none'});
+
+    // open select symbol box
     $('.selectSym').css({'display' : 'block'});
 }
 
+/**
+ * Select the symbol to play.
+ * X goes first.
+ * @param sym X or O.
+ */
 function selectSym(sym) {
     huPlayer = sym;
     aiPlayer = sym === 'O' ? 'X' : 'O';
 
+    // close select symbol box
     $('.selectSym').css({'display' : 'none'});
 
+    // enable cursor for cells
+    cells.css({'cursor' : 'pointer'});
+
+    // let AI move first if ai chooses X
+    if (aiPlayer === 'X') {
+        // AI plays in 1 of the corners as they're the strongest openings ỉn 3x3.
+        // human must play the center cell to get a draw.
+        turn([0, 2, 6, 8][Math.floor(Math.random()*4)], aiPlayer);
+
+        console.log(numNodes);
+        numNodes = 0;
+    }
+
+    // switch between players when an empty cell is click on
     cells.on("click", function(event) {
         if (typeof origBoard[event.target.id] === 'number') {
+            // human chooses a cell
             turn(event.target.id, huPlayer);
+
+            // AI chooses a cell if game isn't over
             if (!checkWin(origBoard, huPlayer) && !checkTie())
-                turn(bestSpot(), aiPlayer);
+                turn(findBestMove(origBoard), aiPlayer);
         }
+
+
         console.log(numNodes);
         numNodes = 0;
     });
 
-    cells.css({'cursor' : 'pointer'});
-
-    if (aiPlayer === 'X') {
-        turn([0, 2, 6, 8][Math.floor(Math.random()*4)], aiPlayer);
-        console.log(numNodes);
-        numNodes = 0;
-    }
 }
 
-function turn(squareId, player) {
-    origBoard[squareId] = player;
-    $('#' + squareId).text(player).css({'color' : player === 'X' ? '#3FC0E0' : '#E95151'});
+/**
+ * Register player's move.
+ * @param cellId The cell's id.
+ * @param player AI or human.
+ */
+function turn(cellId, player) {
+    // put player's symbol on cell
+    origBoard[cellId] = player;
+
+    // set the symbol's color
+    $('#' + cellId).text(player).css({'color' : player === 'X' ? '#3FC0E0' : '#E95151'});
+
+    // check if game is won
     let gameWon = checkWin(origBoard, player);
     if (gameWon)
         gameOver(gameWon);
+
+    // check if game is tie
     checkTie();
 }
 
+/**
+ * Check if a player has won the game.
+ * @param board The board.
+ * @param player AI or human.
+ * @returns {*} Index of the win combo and the winner, otherwise null.
+ */
 function checkWin(board, player) {
-    let plays = board.reduce((a, e, i) => e === player ? a.concat(i) : a, []);
     let gameWon = null;
-    for (let [index, win] of winCombos.entries()) {
-        if (match(win, plays)) {
+
+    // get player's current moves
+    let moves = board.reduce((a, e, i) => e === player ? a.concat(i) : a, []);
+
+    // check if player's moves match any win combo
+    for (let [index, winCombo] of winCombos.entries()) {
+        if (match(winCombo, moves)) {
             gameWon = {index: index, player: player};
             break;
         }
     }
+
     return gameWon;
 }
 
+/**
+ * Check if player's moves match any win combo.
+ * @param winCombo A win combo.
+ * @param moves The moves player has made.
+ * @returns {boolean} True if player's moves match any win combo, otherwise False.
+ */
+function match(winCombo, moves) {
+    return winCombo.every(e => moves.includes(e));
+}
+
+/**
+ * Check if game is a tie. Declare it's a tie if board is full and
+ * no one has won.
+ * @returns {boolean} Return True if game is a tie, otherwise False.
+ */
 function checkTie() {
-    if (!isMoveLeft(origBoard)) {
+    // check if board is full
+    if (!isNotFull(origBoard)) {
+        // highlights all cells
         cells.css({'background-color' : '#414141'});
+
+        // make cells unclickable
         cells.off('click');
+
+        // make cursor normal
+        cells.css({'cursor' : 'default'});
+
+        // declare a tie
         declareWinner("It's a Tie!");
         return true;
     }
+
     return false;
 }
 
-function match(win, plays) {
-    return win.every(e => plays.includes(e));
-}
-
+/**
+ * Bring game to an end.
+ * @param gameWon
+ */
 function gameOver(gameWon) {
+    // color win combo of the winner
     for (let index of winCombos[gameWon.index]) {
         $('#' + index).css({'background-color' : "#414141"});
     }
 
+    // make cells unclickable
     cells.off('click');
+
+    // make cursor normal
     cells.css({'cursor' : 'default'});
+
+    // declare victory or defeat for human
     declareWinner(gameWon.player === huPlayer ? "You win!" : "You lose!");
 }
 
-function declareWinner(who) {
+/**
+ * Declare the winner in end game box.
+ * @param player AI or human.
+ */
+function declareWinner(player) {
+    // add end game text
+    $(".endgame .text").text(player);
+    // open end game box
     $(".endgame").css({'display' : 'block'});
-    $(".endgame .text").text(who);
 }
 
-function bestSpot() {
-    return findBestMove(origBoard, aiPlayer);
-}
-
-// check if there is any move left
-function isMoveLeft(board) {
+/**
+ * Check if board is not full.
+ * @param board The board.
+ * @returns {boolean} True if board is not full, otherwise False.
+ */
+function isNotFull(board) {
     for (let elem of board) {
         if (typeof elem === 'number') {
             return true;
@@ -113,25 +225,39 @@ function isMoveLeft(board) {
     return false;
 }
 
-// evaluate the board
-// return -10 if human player wins
-// return 10 if ai player wins
-// return 0 if
+/**
+ * Evaluate the board when it's full (in terminal state).
+ * @param board The board.
+ * @returns {number} -1 if AI wins, 1 if human wins, 0 if it's a draw.
+ */
 function evaluate(board) {
     if (checkWin(board, huPlayer)) {
         return -10;
     } else if (checkWin(board, aiPlayer)) {
         return 10;
-    } else if (!isMoveLeft(board)) {
+    } else if (!isNotFull(board)) {
         return 0;
     }
 }
 
-// consider all possible ways the game can go and return
-// the value of the board
+/**
+ * Use the minimax algorithm to consider all possible
+ * positions the game can go and return the value of the
+ * board in terminal state. Alpha beta pruning helps
+ * reducing the number of evaluation. Depth helps finding
+ * the shortest path to victory.
+ * @param board The board.
+ * @param player AI or human.
+ * @param depth Depth starts at 0 and increases as we go down
+ * the tree.
+ * @param alpha Best score for AI.
+ * @param beta Best score for human.
+ * @returns {*} Evaluation of a move.
+ */
 function minimax(board, player, depth, alpha, beta) {
     numNodes++;
 
+    // evaluate the current position
     let score = evaluate(board);
 
     // ai player wins
@@ -145,7 +271,7 @@ function minimax(board, player, depth, alpha, beta) {
     }
 
     // a tie
-    if (!isMoveLeft(board)) {
+    if (!isNotFull(board)) {
         return 0;
     }
 
@@ -166,6 +292,7 @@ function minimax(board, player, depth, alpha, beta) {
                 // undo the move
                 board[i] = i;
 
+                // prune with alpha, beta
                 alpha = Math.max(alpha, val);
                 if (beta <= alpha) {
                     break;
@@ -194,6 +321,7 @@ function minimax(board, player, depth, alpha, beta) {
                 // undo the move
                 board[i] = i;
 
+                // prune with alpha, beta
                 beta = Math.min(beta, val);
                 if (beta <= alpha) {
                     break;
@@ -205,15 +333,20 @@ function minimax(board, player, depth, alpha, beta) {
     }
 }
 
-// return index of the best move
-function findBestMove(board, player) {
+/**
+ * Find the best move for AI.
+ * @param board The board.
+ * @returns {number} The index of the move on the board.
+ */
+function findBestMove(board) {
+    // AI starts as the maximizer
     let bestVal = -Infinity;
-    let index = -1;
+    let index;
 
     for (let [i, elem] of board.entries()) {
         if (typeof elem === 'number') {
             // make the move
-            board[i] = player;
+            board[i] = aiPlayer;
 
             // call minimax recursively and choose
             // the minimum value
@@ -229,6 +362,7 @@ function findBestMove(board, player) {
             }
         }
     }
+
     return index;
 }
 
